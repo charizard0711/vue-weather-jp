8272495d8850e4927219b4860ce3f76aea6030b4
charizard0711 <masamitu783@gmail.com>
2025-11-07 23:55:28 +0900

    build for github pages

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,18 @@
+// import { defineConfig } from 'vite'
+// import vue from '@vitejs/plugin-vue'
+
+// // https://vite.dev/config/
+// export default defineConfig({
+//   plugins: [vue()],
+// })
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
-// https://vite.dev/config/
 export default defineConfig({
+  base: './',              // ★ 相対パスにしておく
+  build: {
+    outDir: 'docs',        // ★ ビルド結果を docs フォルダに出す
+  },
   plugins: [vue()],
 })
+
